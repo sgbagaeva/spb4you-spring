b3caaf982bb8920ae3625252465ed6fa43a8fdf9
svetabagaeva5 <sgbagaeva.work@gmail.com>
2025-03-13 00:43:58 +0300

    added @EnableJdbcRepositories

--- src/main/java/com/example/spb4you/Spb4youApplication.java
+++ src/main/java/com/example/spb4you/Spb4youApplication.java
@@ -2,8 +2,10 @@ package com.example.spb4you;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.jdbc.repository.config.EnableJdbcRepositories;
 
 @SpringBootApplication
+@EnableJdbcRepositories
 public class Spb4youApplication {
 
 	public static void main(String[] args) {
